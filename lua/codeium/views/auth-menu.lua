963c4604b52470d9e904a0e6e8421fffa8686d52
marshmallow <marshycity@gmail.com>
2023-06-11 06:38:36 +1000

    Use `inputsecret` for token (#61)

--- lua/codeium/views/auth-menu.lua
+++ lua/codeium/views/auth-menu.lua
@@ -2,7 +2,7 @@ local io = require("codeium.io")
 local notify = require("codeium.notify")
 
 local function get_key(callback)
-	vim.ui.input({
+	vim.ui.inputsecret({
 		prompt = "Token ",
 		on_submit = callback,
 	}, function(result)
