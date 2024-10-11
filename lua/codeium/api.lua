b90a6d3d2dd7a79368c2176e3791a3399703ce02
Daniel Imfeld <daniel@imfeld.dev>
2024-10-11 07:49:18 -1000

    Use relative_path_migrate_me_to_workspace_uri instead of workspace_uri for now

--- lua/codeium/api.lua
+++ lua/codeium/api.lua
@@ -443,7 +443,7 @@ function Server:new()
 			text = text,
 			line_ending = line_ending,
 			absolute_uri = util.get_uri(vim.api.nvim_buf_get_name(bufnr)),
-			workspace_uri = util.get_uri(util.get_relative_path(bufnr)),
+			relative_path_migrate_me_to_workspace_uri = util.get_uri(util.get_relative_path(bufnr)),
 		}
 
 		request("RefreshContextForIdeAction", {
